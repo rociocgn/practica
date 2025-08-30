49683bac022367c4a1497ce5ee4b9cddae49f134
Rocio Cano <rociocgn@gmail.com>
2025-08-30 19:13:45 +0200

    ficheros iniciales

---
+++ ficheroa.js
@@ -0,0 +1,3 @@
+console.log("*****************");
+console.log("Soy el fichero a");
+console.log("*****************");  
---
+++ ficherob.js
@@ -0,0 +1,3 @@
+console.log("****************");
+console.log("Soy el fichero b");
+console.log("****************");
