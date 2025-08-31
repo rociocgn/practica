d0f430081d46b3b3b5ec2b1b7a0969bae46709b4
Rocio Cano <rociocgn@gmail.com>
2025-08-31 10:56:09 +0200

    actualizaciones

--- ficheroa.js
+++
@@ -1,3 +0,0 @@
-console.log("+++++++++++++++++");
-console.log("Soy el fichero a");
-console.log("++++++++++++++++++");  
--- ficherob.js
+++ ficherob.js
@@ -1,3 +1,4 @@
 console.log("________________");
 console.log("Soy el segundo fichero");
 console.log("________________");
+console.log("Entre los pecados mayores que los hombres cometen, aunque algunos dicen que es la soberbia, yo digo que es el desagradecimiento");
